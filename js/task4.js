fa52258650cc7c1029aadf2f18b38eb3a6f9a869
rkukin <romakukin@gmail.com>
2019-08-07 20:35:07 +0300

    task4

--- js/task4.js
+++ js/task4.js
@@ -1,1 +1,28 @@
 "use strict";
+
+function countTotalSalary(obj) {
+  let totalSalary = 0;
+
+  for (const salary of Object.values(obj)) {
+    totalSalary += salary;
+  }
+  return totalSalary;
+}
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150
+  })
+); // 400
