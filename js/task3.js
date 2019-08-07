931ede9c561bca3f4064ec7fe9bc26ad8def648d
rkukin <romakukin@gmail.com>
2019-08-07 19:35:39 +0300

    task3

--- js/task3.js
+++ js/task3.js
@@ -1,1 +1,39 @@
 "use strict";
+
+const findBestEmployee = object => {
+  let highestScore = 0;
+  let bestEmployee;
+  for (const key in object) {
+    if (object[key] >= highestScore) {
+      highestScore = object[key];
+      bestEmployee = key;
+    }
+  }
+  return bestEmployee;
+};
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99
+  })
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4
+  })
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38
+  })
+); // lux
