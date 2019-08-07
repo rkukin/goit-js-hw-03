08600932ae8aee17ecd4e41e9060d260f39cff8e
rkukin <romakukin@gmail.com>
2019-08-07 21:39:30 +0300

    task6

--- js/task6.js
+++ js/task6.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 }
+];
+
+const calculateTotalPrice = (products, name) => {
+  let totalPrice = 0;
+  for (const obj of products) {
+    if (obj.name === name) {
+      totalPrice = obj.price * obj.quantity;
+    }
+  }
+  return totalPrice;
+};
+
+console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+console.log(calculateTotalPrice(products, "Дроид")); // 2800
