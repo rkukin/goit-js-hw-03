629f697b0bddc3ba023c8618cb548721ff675d71
rkukin <romakukin@gmail.com>
2019-08-07 19:20:27 +0300

    tast1 and task2 done

--- js/task1.js
+++ js/task1.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true
+};
+
+user["mood"] = "happy";
+user["hobby"] = "javascript";
+user["premium"] = false;
+
+for (const key of Object.keys(user)) {
+  console.log(key, " : ", user[key]);
+}
+// for (const key in user) {
+//   console.log(key, " : ", user[key]);
+// }
--- js/task2.js
+++ js/task2.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const countProps = object => {
+  let counter = 0;
+  for (const prop in object) {
+    counter++;
+  }
+  return counter;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
